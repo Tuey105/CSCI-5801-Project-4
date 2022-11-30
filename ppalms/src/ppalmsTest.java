f8f2075d51ac9c26b5c6b73d1aaad8ec79851c56
yiyli <119348350+yiyli@users.noreply.github.com>
2022-11-29 21:33:17 -0600

    Update ppalmsTest.java
    add two more unit test cases

--- ppalms/src/ppalmsTest.java
+++ ppalms/src/ppalmsTest.java
@@ -11,18 +11,31 @@ class ppalmsTest {
     void importing() {
         ppalms p = new ppalms();
         ActionEvent e = new ActionEvent(new Object(), 0, "Import");
+        //p.f = new File("/../generatedParsonProblems.txt");
         p.actionPerformed(e);
+        //file loaded figuring out if it's displayed in main screen
         String text = p.t.getText();
         assertNotEquals("",text);
     }
 
+
     @org.junit.jupiter.api.Test
-    void highlighting() {
+    void importing_nonselection() {
         ppalms p = new ppalms();
         ActionEvent e = new ActionEvent(new Object(), 0, "Import");
+        //p.f = new File("/../generatedParsonProblems.txt");
         p.actionPerformed(e);
-        String highlight = p.t.getSelectedText();
-        assertNotEquals("",highlight);
+        //file loaded figuring out if it's displayed in main screen
+        String text = p.t.getText();
+        //check if there are no selection and notified
+        assertEquals("",text);
+    }
+
+    @org.junit.jupiter.api.Test
+    void highlighting() {
+        annotation a = new annotation();
+
+
     }
 
     @org.junit.jupiter.api.Test
@@ -30,11 +43,36 @@ class ppalmsTest {
         ppalms p = new ppalms();
         ActionEvent e = new ActionEvent(new Object(), 0, "Import");
         p.actionPerformed(e);
+        //ask to select code file
         ActionEvent a = new ActionEvent(new Object(), 0, "Generate");
         p.actionPerformed(a);
+        //parson problems generated
         FileReader fr = new FileReader("generatedParsonProblems.txt");
         char[] buf = new char[50];
         fr.read(buf);
+        //read from txt file
         assertNotEquals('\0',buf[1]);
+        //check if empty
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void generating_randomness() throws IOException {
+        ppalms p = new ppalms();
+        ActionEvent e = new ActionEvent(new Object(), 0, "Import");
+        p.actionPerformed(e);
+        //ask to select code file
+        String text = p.t.getText();
+        ActionEvent a = new ActionEvent(new Object(), 0, "Generate");
+        p.actionPerformed(a);
+        //parson problems generated
+        FileReader fr = new FileReader("generatedParsonProblems.txt");
+        char[] buf = new char[50];
+        fr.read(buf);
+        String first = String.valueOf(buf[1]);
+        //check the first element in txt
+        assertNotEquals(text,first);
+        //check if is properly randomized
     }
 }
+
