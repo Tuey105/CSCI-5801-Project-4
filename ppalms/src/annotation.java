a306f5dea08e36ef0a05651ac68b9d7e3431d5e7
Tuey105 <its_jonny@hotmail.com>
2022-11-30 21:55:01 -0600

    Change so no compile error

--- ppalms/src/annotation.java
+++ ppalms/src/annotation.java
@@ -3,19 +3,20 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Highlighter;
 import java.awt.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 // Class for annotating in PPALMS {Design Document 4.2.4}
 public class annotation {
 
     // Arraylist for selected lines {Design Document 4.2.4.1}
-    ArrayList selectedLines;
 
     // Constructor
+    ArrayList<String> selectedLines;
     annotation(){
 
         // Initialization of selectedLines ArrayList
-        selectedLines = new ArrayList<String>();
+        selectedLines = new ArrayList<>();
 
     }
 
